3350d5b344b53d0250304ed4a39049d7287ea168
eggsys <46551235+eggsys@users.noreply.github.com>
2021-07-28 11:33:17 +0700

    add token authentication

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
@@ -13,10 +14,30 @@ const posts = [
         title: 'Post 2'
     }
 ]
-app.get('/post', (req, res) => {
-    console.log("Posts")
-    res.json(posts)
-    res.send("ok!")
+app.get('/post',authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
+
+app.post('/login', (req,res) =>{
+
+    const username = req.body.username
+    const user = {user: username}
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken})
+
+})
+
+function authenticateToken(req, res, nex){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user ) =>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(5000)
