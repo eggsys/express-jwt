0c9ece9c75e1d258010aa11579cc270f23fac597
eggsys <46551235+eggsys@users.noreply.github.com>
2021-07-28 17:31:21 +0700

    update auth

--- server.js
+++ server.js
@@ -18,17 +18,9 @@ app.get('/post',authenticateToken, (req, res) => {
     res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.post('/login', (req,res) =>{
 
-    const username = req.body.username
-    const user = {user: username}
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken})
-
-})
-
-function authenticateToken(req, res, nex){
+function authenticateToken(req, res, next){
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.sendStatus(401)
